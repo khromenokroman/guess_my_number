f6bc6b0ffe75fa0b3a2c7827a1fb20d9bc46c081
khromenokroman <roma55592@yandex.ru>
2023-04-19 08:30:31 +0200

    add random number

--- pc_work.cpp
+++ pc_work.cpp
@@ -2,6 +2,7 @@
 
 void create_pc_number(int &pc_number)
 {
+    srand(static_cast<unsigned int>(time(0)));
     pc_number = rand() % 100;
     std::cout << "Я загадал число: " << pc_number << std::endl;
 }
