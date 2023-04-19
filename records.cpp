521c8947f20ced6f8e560792a65acdfc359cf853
khromenokroman <roma55592@yandex.ru>
2023-04-19 22:25:28 +0200

    remove massiv

--- records.cpp
+++ records.cpp
@@ -13,30 +13,16 @@ int get_count_line()
 
 void get_results()
 {
-    const int MAX_RECORDS_ITEMS = get_count_line();
-    int records_line = 0;
     std::fstream file_in;
     std::string file_in_line;
-    std::string records_items[MAX_RECORDS_ITEMS];
 
+     system("clear");
     file_in.open("records.txt", std::fstream::in);
     for (file_in_line; getline(file_in, file_in_line);)
     {
-        if (records_line == MAX_RECORDS_ITEMS)
-            break;
-        records_items[records_line] = file_in_line;
-        records_line++;
+        std::cout << file_in_line << std::endl;
     }
     file_in.close();
-
-    system("clear");
-
-    for (int i = 0; i < MAX_RECORDS_ITEMS; i++)
-    {
-        if (records_items[i].size() == 0)
-            continue;
-        std::cout << records_items[i] << std::endl;
-    }
 }
 
 void write_records(const std::string &name_user, const int &attempt)
