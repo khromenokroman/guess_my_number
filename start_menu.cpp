68d4251279c40752d5db9bc1c4786ca64bd562a9
khromenokroman <roma55592@yandex.ru>
2023-04-19 22:16:59 +0200

    add check

--- start_menu.cpp
+++ start_menu.cpp
@@ -1,19 +1,35 @@
 #include <iostream>
+#include <unistd.h>
 
 void choice_menu_item(int &answer)
 {
     while (true)
     {
-        std::cout << "***********************************"<< std::endl;
-        std::cout << "\tИГРА УГАДАЙ ЧИСЛО!!!"<< std::endl;
-        std::cout << "***********************************"<< std::endl;
-        std::cout << "1.Начать играть"<< std::endl;
-        std::cout << "2.Просмотреть результаты"<< std::endl;
-        std::cout << "3.Выход"<< std::endl;
-        std::cout << "***********************************"<< std::endl;
+        system("clear");
+        std::cout << "***********************************" << std::endl;
+        std::cout << "\tИГРА УГАДАЙ ЧИСЛО!!!" << std::endl;
+        std::cout << "***********************************" << std::endl;
+        std::cout << "1.Начать играть" << std::endl;
+        std::cout << "2.Просмотреть результаты" << std::endl;
+        std::cout << "3.Выход" << std::endl;
+        std::cout << "***********************************" << std::endl;
         std::cout << "Сделайте выбор: ";
         std::cin >> answer;
-        break;
+        if (std::cin.good())
+        {
+            if (answer <= 0 || answer > 3)
+            {
+                std::cout << "Вы ввели не правильные данные!!!" << std::endl;
+                sleep(1);
+                continue;
+            }
+            break;
+        }
+        else
+        {
+            std::cout << "Вы ввели буквы, надо цифры!!" << std::endl;
+            sleep(1);
+            break;
+        }
     }
-    
 }
